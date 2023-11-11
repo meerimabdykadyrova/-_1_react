f79d31818c1f8822394dac954c3770d30d62244f
meerimabdykadyrova <141637626+meerimabdykadyrova@users.noreply.github.com>
2023-11-11 11:09:17 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, {useState} from "react";
+
+
+export default function MyApp(){
+    const [count, setCount] = useState(0)
+
+    function handleClick(){
+        setCount(count + 1)
+    }
+
+    return(
+        <div>
+            <h1>Button</h1>
+            <MyButton count={count} onClick={handleClick}/>
+            <MyButton count={count} onClick={handleClick}/>
+        </div>
+    )
+}
+
+function MyButton({count, onClick}){
+    return<button onClick={onClick}>Clicked {count} times!</button>
+}
